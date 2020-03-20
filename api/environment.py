9c499e44594a52c1a18d86c0e7cfa47e43b57f44
Piotr Persona <persona.piotr@gmail.com>
2020-03-20 20:13:42 +0100

    Define rabbit and channel environment
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- api/environment.py
+++ api/environment.py
@@ -1,13 +1,49 @@
 import os
 
-APP_PORT = "APP_PORT"
+
+class Channel:
+    EXCHANGE = "CHANNEL_EXCHANGE"
+    TOPIC = "CHANNEL_TOPIC"
+
+    def __init__(self, exchange, topic):
+        self.exchange = exchange
+        self.topic = topic
+
+
+class Rabbit:
+    HOST = "RABBIT_HOST"
+    PORT = "RABBIT_PORT"
+    CONNECTION_ATTEMPTS = "RABBIT_CONNECTION_ATTEMPTS"
+    RETRY_DELAY = "RABBIT_RETRY_DELAY"
+
+    def __init__(self, host, port, connection_attempts, retry_delay):
+        self.host = host
+        self.port = port
+        self.connection_attempts = connection_attempts
+        self.retry_delay = retry_delay
 
 
 class Environment:
-    def __init__(self, port="8080"):
+    PORT = "PORT"
+
+    def __init__(self, rabbit: Rabbit, channel: Channel, port="8080"):
         self.port = port
+        self.rabbit = rabbit
+        self.channel = channel
 
 
 def read_environment() -> Environment:
-    port = os.environ.get(APP_PORT, "8080")
-    return Environment(port=port)
+    port = os.environ.get(Environment.PORT, "8080")
+    return Environment(
+        port=port,
+        rabbit=Rabbit(
+            host=os.environ.get(Rabbit.HOST),
+            port=os.environ.get(Rabbit.PORT),
+            connection_attempts=int(os.environ.get(Rabbit.CONNECTION_ATTEMPTS)),
+            retry_delay=int(os.environ.get(Rabbit.RETRY_DELAY))
+        ),
+        channel=Channel(
+            exchange=os.environ.get(Channel.EXCHANGE),
+            topic=os.environ.get(Channel.TOPIC)
+        )
+    )
